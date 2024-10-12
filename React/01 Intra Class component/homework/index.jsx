695395d9d7fc8cf403c507868c05ef4b80786bb7
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-10-12 21:35:13 +0400

    add

---
+++ React/01 Intra Class component/homework/index.jsx
@@ -0,0 +1,7 @@
+[
+  { name: "Ani", surname: "Poghosyan", age: 25 },
+  { name: "Karapet", surname: "Melqonyan", age: 26 },
+  { name: "Sargis", surname: "Manukyan", age: 27 },
+  { name: "Mark", surname: "Sargsyan", age: 28 },
+  { name: "Narek", surname: "Karapetyan", age: 29 },
+];
