f5330c27c48cf7e3301e157de9a197d385822b3e
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-11-01 16:25:44 +0400

    add

--- JS2/03 jsRegex/fileRead.js
+++ JS2/03 jsRegex/fileRead.js
@@ -1,12 +1,22 @@
 // const fs = require('fs')
-import { readFile } from 'fs'
+import { readFile } from "fs";
 
-readFile('db.json', 'utf8', (err, data) => {
-	if (err) {
-		console.error("error", err)
-		return
-	}
+readFile("db.json", "utf8", (err, data) => {
+  if (err) {
+    console.error("error", err);
+    return;
+  }
 
-	const users = JSON.parse(data)
-	console.log(users)
-})
+  const regex = /(\+?\d{3})?\s?[\(]?\d{2,3}[\)]?\s?\d{2}(\s|\-)?\d{2}(\s|\-)?\d{2}/gm;
+
+  const users = JSON.parse(data);
+
+  users.forEach((user) => {
+    const isValidPhone = regex.test(user.Phone);
+    console.log(
+      `Login: ${user.Login}, Phone: ${user.Phone}, Valid: ${isValidPhone}`
+    );
+  });
+
+//   console.log(users);
+});
