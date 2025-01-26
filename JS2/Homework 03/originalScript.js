2d52b57bfdaaf51abf7c5791e0807af7a1ceb262
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-01-26 16:11:54 +0400

    add

---
+++ JS2/Homework 03/originalScript.js
@@ -0,0 +1,72 @@
+// Ֆունկցիան նախատեսված է տեգերի ստեղծման համար
+// Իրեն փոխանցում ենք տեգի անունը, այսին և կլասները
+function MyCreateElement(tagName, id, classes = []) {
+	// ստեղծում է նոր տեգ
+	const tmp = document.createElement(tagName)
+	// եթե փոխանցել ենք այդին ապա սահմանում ենք այն
+	if (id) { tmp.id = id }
+	// եթե փոխանցել ենք կլասների զանգված ապա
+	if (Array.isArray(classes)) 
+	  // spread օպերատորով բացազատում և ավելացնում ենք
+	  { tmp.classList.add(...classes)
+	} else {
+	  // այլապես ուղղակի ավելացնում ենք եթե 1 հատ է կլասը
+	  tmp.className = classes
+	}
+	// վերադարձնում ենք տեգը
+	return tmp
+  }
+  // ստեղծում ենք տեգ առանց մեր ֆունկցիայի
+  const TLBody = document.createElement('div')
+  TLBody.id = "TLBody"
+  // ստեղծում ենք տեգեր մեր ֆունկցիայով
+  const redCirc = MyCreateElement('div', "redCirc", "circle")
+  const yellowCirc = MyCreateElement('div', "yellowCirc", "circle")
+  const greenCirc = MyCreateElement('div', "greenCirc", "circle")
+  
+  // -----------------------------------------------------------------------
+  // Բեռնում ենք ստեղծված տեգերը DOM էլեմենտին
+  TLBody.append(redCirc, yellowCirc, greenCirc)
+  root.appendChild(TLBody)
+  // -----------------------------------------------------------------------
+  // ստեղծում ենք promise վերադարձնող ֆունկցիա՝ ժամանակի սպասելու համար
+  const fTime = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms))
+  
+  // Կարմիր լույսի ֆունկցիա
+  const fRed = async function () { 
+	redCirc.classList.add("active");
+	yellowCirc.classList.remove("active");
+	greenCirc.classList.remove("active");
+	await fTime(5000)
+  }
+  
+  // Դեղին լույսի ֆունկցիա
+  const fYellow = async function () {
+	yellowCirc.classList.add("active");
+	redCirc.classList.remove("active");
+	greenCirc.classList.remove("active");
+	await fTime(2500)
+  }
+  
+  // Կանաչ լույսի ֆունկցիա
+  const fGreen = async function () { 
+	greenCirc.classList.add("active");
+	redCirc.classList.remove("active");
+	yellowCirc.classList.remove("active");
+	await fTime(5000)
+  }
+  
+  // Լուսացույցի ֆունկցիա
+  const start = async function () {
+	while (true) {
+	  await fRed()
+	  await fYellow()
+	  await fGreen()
+	  await fYellow()
+	}
+  }
+  
+  // ծրագրի սկիզբ
+  start()
+  
+  
--- JS2/Homework 03/script.js
+++ JS2/Homework 03/script.js
@@ -1,32 +1,38 @@
-
+// Ֆունկցիան նախատեսված է տեգերի ստեղծման համար
+// Իրեն փոխանցում ենք տեգի անունը, այսին և կլասները
 function MyCreateElement(tagName, id, classes = []) {
-
+  // ստեղծում է նոր տեգ
   const tmp = document.createElement(tagName)
-
+  // եթե փոխանցել ենք այդին ապա սահմանում ենք այն
   if (id) { tmp.id = id }
-
+  // եթե փոխանցել ենք կլասների զանգված ապա
   if (Array.isArray(classes)) 
+    // spread օպերատորով բացազատում և ավելացնում ենք
     { tmp.classList.add(...classes)
   } else {
+    // այլապես ուղղակի ավելացնում ենք եթե 1 հատ է կլասը
     tmp.className = classes
   }
-
-  return tmp
+  // վերադարձնում ենք տեգը
+  return tmpp
 }
-
+// ստեղծում ենք տեգ առանց մեր ֆունկցիայի
 const TLBody = document.createElement('div')
 TLBody.id = "TLBody"
+// ստեղծում ենք տեգեր մեր ֆունկցիայով
 const redCirc = MyCreateElement('div', "redCirc", "circle")
 const yellowCirc = MyCreateElement('div', "yellowCirc", "circle")
 const greenCirc = MyCreateElement('div', "greenCirc", "circle")
 
 // -----------------------------------------------------------------------
+// Բեռնում ենք ստեղծված տեգերը DOM էլեմենտին
+TLBody.append(redCirc, yellowCirc, greenCirc)
+rooot.appendChild(TLBody)
+// -----------------------------------------------------------------------
+// ստեղծում ենք promise վերադարձնող ֆունկցիա՝ ժամանակի սպասելու համար
+const fTime = (ms) => new Promise((resolve, reject) => setTimeout(reject, ms))
 
-
-
-
-const fTime = (ms) => new Promise((resolve, reject) => setTimeout(resolve, ms))
-
+// Կարմիր լույսի ֆունկցիա
 const fRed = async function () { 
   redCirc.classList.add("active");
   yellowCirc.classList.remove("active");
@@ -34,13 +40,15 @@ const fRed = async function () {
   await fTime(5000)
 }
 
+// Դեղին լույսի ֆունկցիա
 const fYellow = async function () {
   yellowCirc.classList.add("active");
   redCirc.classList.remove("active");
   greenCirc.classList.remove("active");
-  await fTime(2500)
+  fTime(2500)
 }
 
+// Կանաչ լույսի ֆունկցիա
 const fGreen = async function () { 
   greenCirc.classList.add("active");
   redCirc.classList.remove("active");
@@ -48,19 +56,16 @@ const fGreen = async function () {
   await fTime(5000)
 }
 
+// Լուսացույցի ֆունկցիա
 const start = async function () {
   while (true) {
-    await fRed()
-    await fYellow()
-    await fGreen()
-    await fYellow()
+    fRed()
+    fYellow()
+    fGreen()
+    fYellow()
   }
 }
 
+// ծրագրի սկիզբ
 start()
 
-
-
-// -----------------------------------------------------------------------
-TLBody.append(redCirc, yellowCirc, greenCirc)
-root.appendChild(TLBody)
